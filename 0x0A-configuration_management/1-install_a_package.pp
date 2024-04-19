07b8e3a22bc625ef10b398f559a84ad0b89f2864
Neshbobo <wambuineema27@gmail.com>
2024-04-19 05:32:07 -0700

    1

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -3,3 +3,11 @@ package { 'Flask':
   ensure   => '2.1.0',
   provider => 'pip',
 }
+package { 'python3-pip':
+  ensure => installed,
+}
+
+exec { 'update-alternatives':
+  command => '/usr/bin/python3.8 /usr/bin/python3',
+  onlyif  => 'alternatives --display python | grep -q "link currently points to /usr/bin/python3.8"',
+}
