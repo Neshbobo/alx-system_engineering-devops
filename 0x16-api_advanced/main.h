f15c2ad7995f6d704f6186c17536d3a6cee5af37
Neshbobo <wambuineema27@gmail.com>
2024-08-15 13:58:54 +0000

    main.h

---
+++ 0x16-api_advanced/main.h
@@ -0,0 +1,11 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+#!/usr/bin/python3
+
+def number_of_subscribers(subreddit)
+def top_ten(subreddit)
+def recurse(subreddit, hot_list=[])
+def count_words(subreddit, word_list)
+
+#endif
